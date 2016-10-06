12665d5e3ee029e038e0a012c0ea5fe5b28da39c
David Ayala and Josh Huffman <david@>
2016-10-06 14:33:20 -0700

    tests pass, refactored team tests

--- tests/TeamTest.php
+++ tests/TeamTest.php
@@ -5,6 +5,7 @@
     */
 
     require_once "src/Team.php";
+    require_once "src/User.php";
     $server = 'mysql:host=localhost;dbname=fantasy_test';
     $username = 'root';
     $password = 'root';
@@ -15,18 +16,22 @@
         protected function tearDown()
         {
           Team::deleteAll();
+          User::deleteAll();
         }
 
         function testGetOwner()
         {
-          $test_team = new Team("Phil Smith", "Awesome Team", 1);
+          $user = new User("Joe", "password");
+          $user->addUser();
+          $test_team = new Team($user->getId(), "Awesome Team");
+          $test_team->save();
           $result = $test_team->getOwner();
-          $this->assertEquals("Phil Smith", $result);
+          $this->assertEquals("Joe", $result);
         }
 
         function testSave()
         {
-          $test_team = new Team("Phil Smith", "Awesome Team", 1);
+          $test_team = new Team(1, "Awesome Team");
           $test_team->save();
           $result = Team::getAll();
           $this->assertEquals([$test_team], $result);
@@ -34,9 +39,9 @@
 
         function testGetAll()
         {
-          $test_team = new Team("Phil Smith", "Awesome Team", 1);
+          $test_team = new Team(1, "Awesome Team");
           $test_team->save();
-          $test_team2 = new Team("Jane Doe", "Super Awesome Team", 2);
+          $test_team2 = new Team(2, "Super Awesome Team");
           $test_team2->save();
           $result = Team::getAll();
           $this->assertEquals([($test_team), ($test_team2)], $result);
